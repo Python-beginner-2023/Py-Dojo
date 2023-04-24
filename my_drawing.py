af124d0cedf3de3d87a62b07e888132a490f7f4b
thiswind <thiswind@gmail.com>
2023-04-24 09:38:12 +0000

    add

---
+++ my_drawing.py
@@ -0,0 +1,30 @@
+import turtle as t
+
+def cube(t, x, y, w, h, hand):
+    t.goto(x, y)
+    t.setheading(0)
+
+    if hand == 'right':
+        t.forward(w)
+        t.left(90)
+        t.forward(h)
+        t.left(90)
+        t.forward(w)
+        t.left(90)
+        t.forward(h)
+        t.left(90)
+    elif hand == 'left':
+        t.forward(w)
+        t.right(90)
+        t.forward(h)
+        t.right(90)
+        t.forward(w)
+        t.right(90)
+        t.forward(h)
+        t.right(90)
+    else:
+        print(f'手性错误: {hand}')
+
+t.setup(900,600)
+cube(t, 10, 10, 300, 200, 'left')
+t.done()
